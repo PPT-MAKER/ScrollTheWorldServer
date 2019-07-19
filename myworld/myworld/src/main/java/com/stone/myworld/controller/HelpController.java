9d7489d3777945805ea1b664fe41def723c4c721
刘磊 <liulei@feifanuniv.com>
2019-07-19 13:53:07 +0800

    add

--- myworld/myworld/src/main/java/com/stone/myworld/controller/HelpController.java
+++ myworld/myworld/src/main/java/com/stone/myworld/controller/HelpController.java
@@ -1,11 +1,7 @@
 package com.stone.myworld.controller;
 
-import java.io.IOException;
-
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -16,13 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.stone.myworld.HomeController;
+import com.stone.myworld.controller.response.StoneResponce;
+import com.stone.myworld.util.Constant;
 
 @Controller
 public class HelpController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	@RequestMapping(value = "/help", method = RequestMethod.GET)
-	public void help(HttpServletRequest httpRequest) {
+	public StoneResponce<Object> help(HttpServletRequest httpRequest) {
 //		String link = httpRequest.getParameter("link");
 //		link = link.split("?")[1];
 //		Map map = new HashMap<String, String>();
@@ -33,28 +31,23 @@ public class HelpController {
 //				break;
 //			}
 //		}
+		StoneResponce<Object> responce = new StoneResponce<>();
 		String token = httpRequest.getParameter("token");
 		logger.info("The token is " + token);
 		String url = "https://h42score.webapp.163.com/help?token=" + token + "&_=1561550220913&callback=jsonp6";
 		CloseableHttpClient client = HttpClients.createDefault();
-		CloseableHttpResponse response = null;
 		HttpGet httpGet = new HttpGet(url);
 		try {
-			for (int i = 1; i < 35; i++) {
-				String ip = "13.2.1." + i;
+			for (int i = 1; i < 3; i++) {
+				String ip = "13.3.1." + i;
 				httpGet.setHeader("X-Forwarded-For", ip);
-				response = client.execute(httpGet);
+				client.execute(httpGet);
 			}
-		} catch (ClientProtocolException e) {
+		} catch (Exception e) {
+			responce.setHr(Constant.FAILURE);
 			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				response.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
+		responce.setHr(Constant.SUCCESS);
+		return responce;
 	}
 }
--- myworld/myworld/src/main/webapp/WEB-INF/views/home.jsp
+++ myworld/myworld/src/main/webapp/WEB-INF/views/home.jsp
@@ -6,11 +6,12 @@
 	<meta http-equiv="Content-Type" content="text/html charset=utf-8">
 	<title>clx助力工具测试版</title>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+	
 </head>
 <body>
 	Token:
     <input id="token" type="text">
-    <br>
+    
     <button type="button">submit</button>
     <p>使用方法：将助力链接中的page_key填入以上文本区并点击提交。示例：若助力链接为https://clx.163.com/201906/czch/?page_key=JpNhZtV0wHkrqwHFOjGl6，则需要填入JpNhZtV0wHkrqwHFOjGl6</p>
 </body>
@@ -18,7 +19,13 @@
 	$("button").click(function(){
 		var tokenStr = document.getElementById("token").value;
 		console.log(tokenStr)
-	    $.get("/myworld/help?token=" + tokenStr);
+	    $.get("/myworld/help?token=" + tokenStr, function(data){
+	    	if(data.hr == 0){
+	    		alert('助力成功');
+	    	} else {
+	    		alert('助力失败，请重试')
+	    	}
+	    });
 	});
 </script>
 </html>
